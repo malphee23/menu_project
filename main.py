eed06fb90e08b298981123b9ee5958b62b2878e5
XD307 <zhora.egorov.2004.egorov@gmail.com>
2025-12-06 11:46:10 +0800

    Простое приложение с книгами

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -21,10 +22,44 @@ books = [
 ]
 
 
-@app.get("/books")
-
+@app.get(
+    "/books",
+    tags=["Книги"],
+    summary="Получиться все книги"
+)
 def read_books():
     return books
+
+@app.get(
+    "/books/{book_id}",
+    tags=["Книги"],
+    summary="Получить книгу"
+)
+def get_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            return book
+        raise HTTPException(status_code=404, detail="Book not found")
+    return None
+
+
+class NewBook(BaseModel):
+    title: str
+    author: str
+
+@app.post(
+    "/books",
+    tags=["Добавление"],
+    summary="Добавить книгу"
+)
+def create_book(new_book: NewBook):
+    books.append({
+            "id": len(books) + 1,
+            "title": new_book.title,
+            "author": new_book.author,
+        })
+    return {"success": True}
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
