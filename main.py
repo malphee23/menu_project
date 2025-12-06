7ea5676a5be1833b2c95e5195527f4dd0ffb8e28
XD307 <zhora.egorov.2004.egorov@gmail.com>
2025-12-06 16:56:33 +0800

    Learning DB functional

--- main.py
+++ main.py
@@ -5,25 +5,36 @@
 import uvicorn
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel, Field, EmailStr
-from sqlalchemy.ext.asyncio import create_async_engine, async_session
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 app = FastAPI()
 
-users = []
+engine = create_async_engine('sqlite+aiosqlite:///users.db')
 
-class UserSchema(BaseModel):
-    email: EmailStr
-    login: str | None
-    password: str = Field(..., min_length=3, max_length=16)
+new_session = async_sessionmaker(engine, expire_on_commit=False)
 
-@app.post("/users")
-def add_user(user: UserSchema):
-    users.append(user)
-    return {"ok": True, "msg": "user added"}
+async def get_session():
+    async with new_session() as session:
+        yield session
 
-@app.get("/users")
-def get_users():
-    return users
+class Base(DeclarativeBase):
+    pass
 
+class UserModel(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email: Mapped[str]
+    password: Mapped[str]
+
+@app.post("/setup_db")
+async def setup_db():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
+    return {"success": True}
+
+#Обязательно нужна
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
