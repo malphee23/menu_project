53db4bd38fead774bb24ea13dd65f409c91d762e
XD307 <zhora.egorov.2004.egorov@gmail.com>
2025-12-06 14:12:38 +0800

    Nothing

--- main.py
+++ main.py
@@ -1,30 +1,15 @@
-import uvicorn
-from fastapi import FastAPI
-
-app = FastAPI()
-
 #python main.py - для запуска локального сайта в терминале PyCharm или cmd
 #http://127.0.0.1:8000/docs - интерактивная документация
 #Ip и порт будет показан в терминале или cmd
 
-books = [
-    {
-        "id": 1,
-        "title": "Python Programming",
-        "author": "Egor",
-    },
-    {
-        "id": 2,
-        "title": "Lol",
-        "author": "Vova",
-    }
-]
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
-
-@app.get("/books")
-
-def read_books():
-    return books
+app = FastAPI(
+    title="Restaurant API",
+    description="API for restaurant service",
+)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
